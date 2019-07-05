8382870ed0437de45f7c1d2a42dd6072fafd7a7a
Kumar Mallikarjuna <kumarmallikarjuna1@gmail.com>
2019-07-05 13:47:15 +0530

    Phase II

--- plugin.py
+++ plugin.py
@@ -6,77 +6,200 @@ from app.plugins.views import TaskView
 from django.shortcuts import render
 from django import forms
 
-from PIL import Image
+from PIL import Image, ImageEnhance
+from PIL import ImageOps
 import io
 import uuid
 import os
+import glob
 import base64
 import subprocess
 
 import numpy as np
+import cv2
 
-raw_image = None
+import matplotlib.pyplot as plt
+
+fnames = []
+raw_images = []
+temperature_images = []
 temperature_image = None
+hot_temperature_image = None
+
+temp_range = False
+temp_min = temp_max = 0
 
 class FileForm(forms.Form):
-    file_field = forms.FileField(label='Load Image')
+    file_field = forms.FileField(widget = forms.ClearableFileInput(attrs = {'multiple' : True}))
+
+class TempForm(forms.Form):
+    min_field = forms.DecimalField()
+    max_field = forms.DecimalField()
+
+class TempTaskView(TaskView):
+    def post(self, request, pk=None):
+        global temp_range, temp_min, temp_max
+        
+        temp_max_input = request.POST['max_field']
+        temp_min_input = request.POST['min_field']
+
+        if(temp_max_input != "" and temp_max_input != ""):
+            temp_range = True
+            temp_max = float(temp_max_input)
+            temp_min = float(temp_min_input)
+        else:
+            temp_range = False
+
+        return Response("", status=status.HTTP_200_OK)
 
 class ProcessTaskView(TaskView):
     def post(self, request, pk=None):
-        buff = request.FILES['file_field']
+        files = glob.glob("plugins/temperature/public/tmp/*")
+        for f in files:
+            os.remove(f)
+
+        global fnames, raw_images, temperature_images, temperature_image
+        global temp_min, temp_max, temp_range
+        fnames = []
+        raw_images = []
+        temperature_images = []
+        temperature_image = None
+        buff_list = request.FILES.getlist('file_field')
+
+        if(not temp_range):
+            temp_min = temp_max = 0
+
+        def DN_to_temp(x):
+            y = (x-7593)/25.0
+            y = y.clip(min=0)
+            return y
 
         try:
-            fname = "%s.jpg" % uuid.uuid4()
-            with open("plugins/temperature/public/tmp/" + fname, "wb+") as f:
-                for chunk in buff.chunks():
-                    f.write(chunk)
+            for buff in buff_list:
+                fname = "%s.jpg" % uuid.uuid4()
+                fnames.append(fname)
+                with open("plugins/temperature/public/tmp/" + fname, "wb+") as f:
+                    for chunk in buff.chunks():
+                        f.write(chunk)
 
-            encoded = subprocess.run(["exiftool", "-b", "-j", "plugins/temperature/public/tmp/" + fname], stdout=subprocess.PIPE).stdout.decode("utf-8")
-            encoded = encoded[encoded.index("RawThermalImage\": "):]
-            encoded = encoded[26:encoded.index("\",")]
+                encoded = subprocess.run(["exiftool", "-b", "-j", "plugins/temperature/public/tmp/" + fname], stdout=subprocess.PIPE).stdout.decode("utf-8")
+                encoded = encoded[encoded.index("RawThermalImage\": "):]
+                encoded = encoded[26:encoded.index("\",")]
 
-            processed_fname = "processed_%s" % fname
-
-            with open("plugins/temperature/public/tmp/" + processed_fname, "wb") as f:
-                f.write(base64.b64decode(encoded))
+                processed_fname = "processed_%s" % fname
+                with open("plugins/temperature/public/tmp/" + processed_fname, "wb") as f:
+                    f.write(base64.b64decode(encoded))
             
-            global raw_image, temperature_image
-            raw_image = Image.open("plugins/temperature/public/tmp/" + processed_fname)
-
-            def DN_to_temp(x):
-                y = (x-7593)/25.0
-
-                y = y.clip(min=0)
+                raw_images.append("plugins/temperature/public/tmp/" + processed_fname)
                 
-                return y
+                temperature_image = DN_to_temp(np.int32(Image.open("plugins/temperature/public/tmp/" + processed_fname)))
+                temperature_images.append(temperature_image)
 
-            if(raw_image != None):
-                temperature_image = DN_to_temp(np.int32(raw_image))
-                temperature_image_plot = np.uint8(((temperature_image-temperature_image.min()) / (temperature_image.max() - temperature_image.min())) * 255)
+
+                if(not temp_range):
+                    temp_min = temp_min if temp_min < temperature_image.min() else temperature_image.min()
+                    temp_max = temp_max if temp_max > temperature_image.max() else temperature_image.max()
+
+            for i in range(len(buff_list)):
+#                print(temp_min, temp_max)
+                fname = fnames[i]
+                temperature_image = temperature_images[i]
+                temperature_image_plot = np.int32(((temperature_image-temp_min) / float(temp_max - temp_min)) * 255.0)
+                temperature_image_plot[temperature_image_plot < 0] = 0
+                temperature_image_plot[temperature_image_plot > 255] = 255
+                temperature_image_plot = np.uint8(temperature_image_plot)
                 Image.fromarray(temperature_image_plot).save("plugins/temperature/public/tmp/temperature_" + fname)
-                Image.open("plugins/temperature/public/tmp/" + processed_fname)
+                hot_temperature_image = cv2.applyColorMap(temperature_image_plot, cv2.COLORMAP_HOT)
+                cv2.imwrite("plugins/temperature/public/tmp/hot_temperature_" + fname, hot_temperature_image)
 
-            return Response(fname, status=status.HTTP_200_OK)
+            if(len(raw_images) > 0):
+                if(len(raw_images) == 1):
+                    temperature_image = temperature_images[0]
+                    temperature_image_plot = np.int32(((temperature_image-temp_min) / float(temp_max - temp_min)) * 255.0)
+                    temperature_image_plot[temperature_image_plot < 0] = 0
+                    temperature_image_plot[temperature_image_plot > 255] = 255
+                    temperature_image_plot = np.uint8(temperature_image_plot)
+
+                    fig = plt.figure()
+                    plt.xticks(np.arange(temp_min, temp_max, 5))
+                    ax = plt.gca()
+                    ax.set_title("Histogram (%d bins)" % int(np.floor(temp_max)-np.floor(temp_min)))
+                    plt.xlabel("Temperature (Celsius)")
+                    plt.ylabel("Number of pixels")
+                    ax.set_facecolor('#3498db')
+                    Y, X = np.histogram(temperature_image.ravel(), bins=int(np.floor(temp_max)-np.floor(temp_min)))
+                    cm = plt.cm.get_cmap('hot')
+                    C = [cm(((x-np.floor(temp_min))/(np.floor(temp_max)-np.floor(temp_min)))) for x in X]
+                    plt.bar(X[:-1],Y,color=C,width=X[1]-X[0])
+                    plt.savefig("plugins/temperature/public/tmp/hist_temperature_" + fnames[0])
+
+#                    rav = np.uint8(temperature_image.ravel())
+
+#                    for t in range(int(temp_min), int(temp_max)+1, 5):
+#                        print("Temperature: %dC, Count: %d" % (t, (rav == t).sum()))
+
+                    return Response("temperature_" + fnames[0] + "," + str(temp_min) + "," + str(temp_max), status=status.HTTP_200_OK)
+                else:
+                    images = []
+                    for image in fnames:
+                        images.append(cv2.imread("plugins/temperature/public/tmp/temperature_"+image))
+                    stitcher = cv2.Stitcher_create()
+                    (_, stitched) = stitcher.stitch(images)
+                    if _ == 0:
+                        fname = "%s.jpg" % uuid.uuid4()
+
+                        cv2.imwrite("plugins/temperature/public/tmp/temperature_" + fname, stitched)
+                        hot_temperature_image = cv2.applyColorMap(stitched, cv2.COLORMAP_HOT)
+                        cv2.imwrite("plugins/temperature/public/tmp/hot_temperature_" + fname, hot_temperature_image)
+                        temperature_image = np.array(Image.open("plugins/temperature/public/tmp/temperature_" + fname).convert('L'))
+                        temperature_image_hist = (temperature_image)/255.0*float(temp_max-temp_min) + temp_min
+                        
+                        fig = plt.figure()
+                        plt.xticks(np.arange(temp_min, temp_max, 5))
+                        ax = plt.gca()
+                        ax.set_title("Histogram (%d bins)" % int(np.floor(temp_max)-np.floor(temp_min)))
+                        plt.xlabel("Temperature (Celsius)")
+                        plt.ylabel("Number of pixels")
+                        ax.set_facecolor('#3498db')
+                        Y, X = np.histogram(temperature_image_hist.ravel(), bins=int(np.floor(temp_max)-np.floor(temp_min)))
+                        cm = plt.cm.get_cmap('hot')
+                        C = [cm(((x-np.floor(temp_min))/(np.floor(temp_max)-np.floor(temp_min)))) for x in X]
+                        plt.bar(X[:-1],Y,color=C,width=X[1]-X[0])
+                        plt.savefig("plugins/temperature/public/tmp/hist_temperature_" + fname)
+
+                        return Response("temperature_" + fname + "," + str(temp_min) + "," + str(temp_max), status=status.HTTP_200_OK)
+                    else:
+                        print("Failed to stitch.")
+
+
         except Exception as e:
             print(e)
 
-        return Response("Failed")
+        return Response("Failed to create mosaic.")
 
 class RawTaskView(TaskView):
     def get(self, request, pk=None):
         x = int(float(request.GET['x']))
         y = int(float(request.GET['y']))
+        if len(raw_images) == 1:
+            DN = np.array(Image.open(raw_images[0]))[y, x]
 
-        DN = np.array(raw_image)[y, x]
-
-        return Response("X: %d, Y: %d, DN: %d, Temperature: %0.2fC"
-                                % (
-                                    x,
-                                    y,
-                                    DN,
-                                    temperature_image[y, x]
-                                )
-                , status=status.HTTP_200_OK)
+            return Response("X: %d, Y: %d, DN: %d, Temperature: %0.2fC"
+                                    % (
+                                        x,
+                                        y,
+                                        DN,
+                                        temperature_image[y, x]
+                                    )
+                    , status=status.HTTP_200_OK)
+        else:
+            return Response("X: %d, Y: %d, Temperature: %0.2fC"
+                                    % (
+                                        x,
+                                        y,
+                                        (temperature_image[y, x]/255.0)*(temp_max-temp_min) + temp_min
+                                    )
+                    , status=status.HTTP_200_OK)
 
 class Plugin(PluginBase):
 
@@ -93,15 +216,16 @@ class Plugin(PluginBase):
         return ['component.jsx']
 
     def app_mount_points(self):
-        # Show script only if '?print=1' is set
         def dynamic_cb(request):
             return render(request, self.template_path("app.html"), {
-                'title': 'Test',
-                'form': FileForm()
+                'title': 'Temperature',
+                'form': FileForm(),
+                'temp_form': TempForm()
             })
 
         return [
             MountPoint("$", dynamic_cb),
             MountPoint("process$", ProcessTaskView.as_view()),
             MountPoint("raw$", RawTaskView.as_view()),
+            MountPoint("temp$", TempTaskView.as_view()),
         ]
--- public/main.js
+++ public/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+/*
 	$("#im").mousemove(function(e){
 		if(!this.canvas){
 			this.canvas = $("<canvas>")[0];
@@ -12,12 +13,10 @@ $(document).ready(function(){
 		}
 
 		var pixel = this.canvas.getContext("2d").getImageData(e.offsetX, e.offsetY, 1, 1).data;
-
-		$("#bar").html("R: " + pixel[0]);
 	});
+*/
 
-
-	$("#file_form input[type=file]").change(function(){
+	function foo(){
 		var file_data = new FormData($("#file_form")[0]);
 		$.ajax({
 			type: "POST",
@@ -26,14 +25,51 @@ $(document).ready(function(){
 			processData : false,
 			contentType: false,
 			success: function(x){
-				$("#photo").attr("src", "tmp/temperature_" + x).css("display", "block");
+				if(x != "Failed to create mosaic.") {
+					var l = x.split(",");
+					$("#photo").attr("src", "tmp/hot_" + l.slice(-3, -2)).css("display", "inline");
+					$("#hist").attr("src", "tmp/hist_" + l.slice(-3, -2)).css("display", "inline");
+					$("#colorscale").css("display", "inline");
+					$("#legend_left").html(l.slice(-2, -1) + "&nbsp;");
+					$("#legend_right").html(l.slice(-1));
+					$("#bar").text("");
+				} else {
+					$("#photo").css("display", "none");
+					$("#hist").css("display", "none");
+					$("#colorscale").css("display", "none");
+					$("#legend_left").html("");
+					$("#legend_right").html("");
+					$("#bar").text(x);
+				}
+				/*
 				var ctx = document.getElementById("palette").getContext("2d");
-				var grad = ctx.createLinearGradient(0,0, 640, 0);
+				var grad = ctx.createLinearGradient(0, 0, 640, 0);
 				grad.addColorStop(0, "black");
 				grad.addColorStop(1, "white");
 
 				ctx.fillStyle = grad;
 				ctx.fillRect(0, 0, 640, 100);
+				*/
+			}
+		});
+	}
+
+	$("#file_form input[type=file]").change(function(){
+		foo();
+	});
+
+	$("#temp_form input[type=number]").change(function(){
+		var max_data = $("#id_max_field").val()
+		var min_data = $("#id_min_field").val()
+		$.ajax({
+			type: "POST",
+			url: "temp",
+			data: {
+				'max_field' : max_data,
+				'min_field' : min_data
+			},
+			success: function(){
+				foo();
 			}
 		});
 	});
